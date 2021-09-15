be5f0baeafaf764c42234fd9ebc3149ff8961104
s6135410006 <s6135410006@sau.ac.th>
2021-09-15 16:11:06 +0700

    V.1.1 :

--- lib/views/log_ui.dart
+++ lib/views/log_ui.dart
@@ -17,99 +17,159 @@ class _LoginUIState extends State<LoginUI> {
           child: Column(
             children: [
               SizedBox(
-                height: 80.0,
+                height: 70.0,
               ),
               Image.asset(
-                'assets/images/naegzaasd.pug',
-                //width: 120.0,
-                width: MediaQuery.of(context).size.width * 0.45,
+                "assets/images/education.png",
+                width: MediaQuery.of(context).size.width * 0.5, //
               ),
-              SizedBox(),
+              SizedBox(
+                height: 10.0,
+              ),
               RichText(
                 text: TextSpan(
                   children: [
                     TextSpan(
-                      text: 'DTI',
+                      text: "DTI",
                       style: TextStyle(
-                        fontFamily: 'Kanit',
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
                         color: Colors.purple,
+                        fontFamily: 'Kanit',
+                        fontSize: 37.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                     TextSpan(
-                      text: '-',
+                      text: "-",
                       style: TextStyle(
+                        color: Colors.orange,
                         fontFamily: 'Kanit',
-                        fontSize: 35.0,
+                        fontSize: 37.0,
                         fontWeight: FontWeight.bold,
-                        color: Colors.purple,
                       ),
                     ),
                     TextSpan(
-                      text: 'BC',
+                      text: "BC",
                       style: TextStyle(
-                        fontFamily: 'Kanit',
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    TextSpan(
-                      text: '-',
-                      style: TextStyle(
-                        fontFamily: 'Kanit',
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
                         color: Colors.blue,
+                        fontFamily: 'Kanit',
+                        fontSize: 37.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                     TextSpan(
-                      text: 'IOT',
+                      text: "-",
                       style: TextStyle(
+                        color: Colors.orange,
                         fontFamily: 'Kanit',
-                        fontSize: 35.0,
+                        fontSize: 37.0,
                         fontWeight: FontWeight.bold,
-                        color: Colors.red,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "IoT",
+                      style: TextStyle(
+                        color: Colors.red[800],
+                        fontFamily: 'Kanit',
+                        fontSize: 37.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ],
                 ),
               ),
               Text(
-                'Southeast Asia University',
+                "Southeast Asia University",
                 style: TextStyle(
-                  fontFamily: 'Kanit',
+                  fontFamily: "Kanit",
                   fontSize: 15.0,
                   fontWeight: FontWeight.bold,
                   color: Colors.grey[800],
                 ),
               ),
-              TextField(
-                decoration: InputDecoration(
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1.0,
-                      color: Colors.blue,
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1.0,
+                        color: Color(0xFF101276),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1.0,
+                        color: Color(0xFF101276),
+                      ),
+                    ),
+                    labelText: "ชื่อผู้ใช้",
+                    labelStyle: TextStyle(
+                      fontFamily: "Kanit",
+                      color: Color(0xFF101276),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    hintText: "ภาษาอังกฤษเท่านั้น",
+                    hintStyle: TextStyle(
+                      fontFamily: "Kanit",
+                      color: Colors.grey[350],
+                      fontStyle: FontStyle.italic,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Color(0xFF101276),
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 1.0,
-                      color: Colors.blue,
-                    ),
-                  ),
-                  labelText: 'ชื่อผู้ใช้',
-                  labelStyle: TextStyle(
-                    fontFamily: 'Kanit',
-                    color: Color(0xFF101276),
-                  ),
-                  floatingLabelBehavior: FloatingActionButtonAnimator.always,
-                  hintStyle: TextStyle(),
-                  prefixIcon: Icon(
-                    Icons: person,
-                    color: Color(0xFF101276),
-                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 40.0,
+                  right: 40.0,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.0,
+                          color: Color(0xFF101276),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.0,
+                          color: Color(0xFF101276),
+                        ),
+                      ),
+                      labelText: "รหัสผ่าน",
+                      labelStyle: TextStyle(
+                        fontFamily: "Kanit",
+                        color: Color(0xFF101276),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      hintText: "ไม่ต่ำกว่า 6 ตัวอักษร",
+                      hintStyle: TextStyle(
+                        fontFamily: "Kanit",
+                        color: Colors.grey[350],
+                        fontStyle: FontStyle.italic,
+                      ),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Color(0xFF101276),
+                      ),
+                      suffixIcon: Icon(
+                        Icons.visibility_off,
+                        color: Colors.grey,
+                      )),
+                  obscureText: true,
                 ),
               ),
             ],
